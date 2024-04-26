98b7470f064972d4cf89bd3476a3179658dea51b
Aubrey Nedwin Mantiri <141216701+aubrxyy@users.noreply.github.com>
2024-04-26 12:02:46 +0700

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var navbarToggle = document.querySelector('[data-collapse-toggle="navbar-default"]');
-    var navbar = document.getElementById('navbar-default');
-
-    navbarToggle.addEventListener('click', function () {
-        navbar.classList.toggle('hidden');
-    });
-});
